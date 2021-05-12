c3a4372784042e7beae7aab353064c5ba03ba84a
RM <70502707+reyad0606@users.noreply.github.com>
2021-05-12 12:55:58 -0500

    ..

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Questions from './Questions';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>React Quiz</h1>
+      <Questions />
+    </div>
+  );
+}
+
+export default App;
